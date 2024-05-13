5257b7db445a2779b772a071e835bb3425c3b000
Porcupine116 <ainurrahimov5@gmail.com>
2024-05-13 23:50:26 +0300

    Фикс багов

--- AYEshop/clothes/urls.py
+++ AYEshop/clothes/urls.py
@@ -1,3 +1,6 @@
 from django.urls import path
 from . import views
 
+urlpatterns = [
+    path('', views.clothes_home, name='clothes_home')
+]
--- AYEshop/clothes/views.py
+++ AYEshop/clothes/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 from .models import Articles
 
+
+def clothes_home(request):
+    clothes = Articles.objects.all()
+    return render(request, 'clothes/clothes_home.html', {"clothes": clothes})
