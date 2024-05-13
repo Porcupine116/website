a57857f9c9a25550ff0e461f95b82b425acd5e09
Porcupine116 <ainurrahimov5@gmail.com>
2024-05-13 23:47:22 +0300

    Работа над бэк

--- AYEshop/clothes/urls.py
+++ AYEshop/clothes/urls.py
@@ -1,6 +1,3 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [
-    path('', views.clothes_home, name='clothes_home')
-]
--- AYEshop/clothes/views.py
+++ AYEshop/clothes/views.py
@@ -1,7 +1,3 @@
 from django.shortcuts import render
 from .models import Articles
 
-
-def clothes_home(request):
-    clothes = Articles.objects.all()
-    return render(request, 'clothes/clothes_home.html', {"clothes": clothes})
