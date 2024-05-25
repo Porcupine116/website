a21c496bd5c351980948363a523db82cbaeb2b53
Porcupine116 <ainurrahimov5@gmail.com>
2024-05-25 11:23:46 +0300

    fix links to html

--- AYEshop/clothes/views.py
+++ AYEshop/clothes/views.py
@@ -19,23 +19,23 @@ def clothes_woman(request):
 
 def shoes_man(request):
     clothes = Articles.objects.all()
-    return render(request, 'clothes/clothes_home.html', {"clothes": clothes})
+    return render(request, 'clothes/shoes_man.html', {"clothes": clothes})
 
 
 def shoes_woman(request):
     clothes = Articles.objects.all()
-    return render(request, 'clothes/clothes_home.html', {"clothes": clothes})
+    return render(request, 'clothes/shoes_woman.html', {"clothes": clothes})
 
 
 def shoes_home(request):
     clothes = Articles.objects.all()
-    return render(request, 'clothes/clothes_home.html', {"clothes": clothes})
+    return render(request, 'clothes/shoes_home.html', {"clothes": clothes})
 
 
 def accessories(request):
     clothes = Articles.objects.all()
-    return render(request, 'clothes/clothes_home.html', {"clothes": clothes})
+    return render(request, 'clothes/accessories_home.html', {"clothes": clothes})
 
 
 def new(request):
-    return render(request, 'clothes/clothes_home.html')
+    return render(request, 'clothes/new_home.html')
